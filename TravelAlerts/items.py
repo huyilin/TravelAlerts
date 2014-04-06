75bd9360589aeb69adea258fbe8082994e5d41a0
root <root@yilin-K46CM.(none)>
2014-04-06 16:07:42 +0200

    Description of events tokenized, date formated, cities filerting added

--- TravelAlerts/items.py
+++ TravelAlerts/items.py
@@ -2,12 +2,14 @@
 #
 # See documentation in:
 # http://doc.scrapy.org/en/latest/topics/items.html
-
 from scrapy.item import Item, Field
 
 class TravelAlertsItem(Item):
     # define the fields for your item here like:
     # name = Field()
     event= Field()
-    destination= Field()
+    city= Field()
     date=Field()
+class CityList():
+    citylist=[u'Chile',u'Guinea','Brazil','Rio']
+    
--- TravelAlerts/spiders/spider.py
+++ TravelAlerts/spiders/spider.py
@@ -2,14 +2,40 @@
 from scrapy.spider import Spider
 from TravelAlerts.items import TravelAlertsItem
 from scrapy.selector import Selector
+from TravelAlerts.items import CityList
+import nltk
+import unicodedata
+import time
+
+def convert_date(date):
+    date=time.strptime(date,"%d %b")
+    date=time.strftime("2014-%m-%d",date)
+    return date
+
+def construct(events,dates,areas,citylist):
+    Alerts=[]
+    for event,date,destination in zip(events,dates,areas):
+        keywords=nltk.pos_tag(nltk.word_tokenize(event))
+        for city in destination:
+            #if city in citylist:
+                for keyword in keywords:
+                    if keyword[1]=='NN' or keyword[1]=='NNP':
+                        item=TravelAlertsItem()
+                        item['event']=unicodedata.normalize('NFKD', keyword[0]).encode('ascii','ignore')
+                        item['city']=unicodedata.normalize('NFKD', city).encode('ascii','ignore')
+                        conv=unicodedata.normalize('NFKD', date).encode('ascii','ignore')
+                        item['date']=convert_date(conv)
+                        Alerts.append(item)
+    return Alerts
+
 class TravelAlerts(Spider):
     name = 'TravelAlerts'
     allowed_domains = ['wikitravel.org']
     start_urls = ['http://wikitravel.org/en/Travel_news']
     def parse(self,response):
-        Alerts=[]
         areas=[]
         events=[]
+        CityFilter=CityList()
         sel=Selector(response)
         events_dir=sel.xpath('//span[@class="mw-headline"]')
         dates=sel.xpath('//div[@class="mw-content-ltr"]/table/tr/td/p/i/text()').extract() #should be /table/tbody/tr.... but no applicable
@@ -19,13 +45,7 @@ class TravelAlerts(Spider):
                 events.append(''.join(event))
                 area=dir.xpath('span/a/text()').extract()
                 areas.append(area)
-        for event,date,destination in zip(events,dates,areas):
-            item=TravelAlertsItem()
-            item['event']=event
-            item['date']=date
-            item['destination']=destination
-            Alerts.append(item)
+        Alerts=construct(events,dates,areas,CityFilter.citylist)
         for unit in Alerts:
             print unit
-            print
-        return Alerts    
+        return Alerts
